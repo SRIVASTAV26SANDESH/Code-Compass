d69b0e19e51a50b91b26c2bd852b1c0bf1a8177a
Ashu Yadav <mailtoashuyadav@gmail.com>
2024-05-03 15:20:41 +0530

    bg done

--- app/src/main/java/com/example/codecompass/DashboardActivity.kt
+++ app/src/main/java/com/example/codecompass/DashboardActivity.kt
@@ -1,29 +1,20 @@
 package com.example.codecompass
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SignUpScreen : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up_screen)
+        setContentView(R.layout.activity_dashboard)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val signupButton:Button=findViewById(R.id.signup)
-        signupButton.setOnClickListener {
-            val intent = Intent(this, Login_Activity::class.java)
-            startActivity(intent)
-            finish()
         }
     }
 }
--- app/src/main/java/com/example/codecompass/Login_Activity.kt
+++ app/src/main/java/com/example/codecompass/Login_Activity.kt
@@ -1,6 +1,8 @@
 package com.example.codecompass
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class Login_Activity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val loginButton: Button = findViewById(R.id.login)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
