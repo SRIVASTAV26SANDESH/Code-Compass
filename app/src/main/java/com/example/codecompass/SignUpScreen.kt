31a0607147c58386e485ecdfaac1d89f8bb9cb3e
Ashu Yadav <mailtoashuyadav@gmail.com>
2024-05-03 13:31:21 +0530

    first

---
+++ app/src/main/java/com/example/codecompass/SignUpScreen.kt
@@ -0,0 +1,29 @@
+package com.example.codecompass
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUpScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val signupButton:Button=findViewById(R.id.signup)
+        signupButton.setOnClickListener {
+            val intent = Intent(this, SignUpScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
